8fbca9f0d4de447fb723b0f41a7b38756b226197
Hermes <131220852+HermesBravo@users.noreply.github.com>
2023-06-23 10:25:45 -0400

    Add files via upload

---
+++ Charlie/HACK-1.py
@@ -0,0 +1,9 @@
+#"//vaciar la lista [100,200,300,400,500,600,700] "
+
+def fn_hack_1():
+    lista = [100,200,300,400,500,600,700]
+    lista.clear()
+    print(lista) 
+    return lista
+
+fn_hack_1()  
